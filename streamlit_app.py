2bf78276dd16f034e326bee41caebbcaf32815c3
Carla <112182610+CarlaColCopp@users.noreply.github.com>
2022-08-26 15:57:34 -0500

    Update streamlit_app.py
    2022-08-26: Aprendiendo SnowFlake + Streamlit + GitHUB

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 
 import streamlit
-streamlit.title("Carla says: Me gusta la nueva cena saludable de mis padres!! :)")
+streamlit.title("Carla says: Me gusta la nueva cena saludable de mis padres!! :) 
+                Me gusta el mate ")
