33871921b3597084bde9e2919fd0a00d2259ee9d
Carla <112182610+CarlaColCopp@users.noreply.github.com>
2022-08-26 15:58:40 -0500

    Update streamlit_app.py
    Es error si se ocupa mas de una linea

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
 
 import streamlit
-streamlit.title("Carla says: Me gusta la nueva cena saludable de mis padres!! :) 
-                Me gusta el mate ")
+streamlit.title("Carla says: Me gusta la nueva cena saludable de mis padres!! :) Me gusta el mate ")
